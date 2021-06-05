38a61dee059d4f17baa37bf67b2897f12a267c13
Ramon David <radavidjr@outlook.com>
2021-06-05 17:04:11 +0100

    formatting

--- src/components/CircuitComponents/CircuitHeader.js
+++ src/components/CircuitComponents/CircuitHeader.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const CircuitHeader = ({ details, match }) => {
-  console.log('Details', details);
-  return (
-    <CircuitPageHeader details={details}>
-      <HeaderTitle>
-        <Link to='/racecalendar'>
-          <Icon />
-        </Link>
-        <h1>
-          <Flag src={`https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Flags%2016x9/${mapFlag(details.Circuit.Location.country.toLowerCase())}-flag.png.transform/1col-retina/image.png`} />
-            {details.Circuit.Location.country}
-        </h1>
-      </HeaderTitle>
-    </CircuitPageHeader>
-  );
+const CircuitHeader = ({details, match}) => {
+    console.log('Details', details);
+    return (
+        <CircuitPageHeader details={details}>
+            <HeaderTitle>
+                <Link to='/racecalendar'>
+                    <Icon/>
+                </Link>
+                <h1>
+                    <Flag
+                        src={`https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Flags%2016x9/${mapFlag(details.Circuit.Location.country.toLowerCase())}-flag.png.transform/1col-retina/image.png`}/>
+                    {details.Circuit.Location.country}
+                </h1>
+            </HeaderTitle>
+        </CircuitPageHeader>
+    );
 };
 
 export default CircuitHeader;
 
 const mapFlag = (country) => {
-  switch (country) {
-    case 'uk':
-      return 'great-britain';
-    default:
-      return country;
-  }
-}
-
+    switch (country) {
+        case 'uk':
+            return 'great-britain';
+        default:
+            return country;
+    }
+};
 
 const mapCountry = (country) => {
-  switch (country) {
-    case 'UK':
-      return 'Great%20Britain';
-    default:
-      return country;
-  }
+    switch (country) {
+        case 'UK':
+            return 'Great%20Britain';
+        default:
+            return country;
+    }
 };
 
 const CircuitPageHeader = styled.div`
